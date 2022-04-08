38977df8f1d5909065374a5cae73de7a55c5cb5a
adamicov2 <adam.weber@cobiro.com>
2022-04-08 15:57:15 +0200

    finish ffs

--- src/app/todos/todos-manager.module.ts
+++ src/app/todos/todos-manager.module.ts
@@ -3,14 +3,14 @@ import { TodosContainerComponent } from './adapters/primary/ui/containers/todos-
 import { TodoListItemComponent } from './adapters/primary/ui/presentation/todo-list-item/todo-list-item.component';
 import { TodoCreateWidgetComponent } from './adapters/primary/ui/containers/todo-create-widget/todo-create-widget.component';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AddTodoStateModule } from './application/state/add-todo.state.module';
 import { HttpTodosServiceModule } from './adapters/secondary/http-todos.service-module';
 import { HttpClientModule } from '@angular/common/http';
 import { TodosInMemoryStorageModule } from './adapters/secondary/todos.in-memory-storage.module';
 import { TodoListStateModule } from './application/state/todo-list.state.module';
 import { NgxBoostrapModalHexagonalModule } from '../core/ngx-boostrap-modal-hexagonal/ngx-boostrap-modal-hexagonal.module';
-import {ModalModule} from "ngx-bootstrap/modal";
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @NgModule({
   imports: [
@@ -20,6 +20,7 @@ import {ModalModule} from "ngx-bootstrap/modal";
     HttpTodosServiceModule,
     HttpClientModule,
     TodoListStateModule,
+    FormsModule,
     TodosInMemoryStorageModule,
     NgxBoostrapModalHexagonalModule,
     ModalModule,
